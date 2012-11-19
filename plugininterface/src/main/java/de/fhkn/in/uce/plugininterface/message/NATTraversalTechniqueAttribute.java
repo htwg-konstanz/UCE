de0e554843cc687e9beae1cc54e6e779e0dd7daa
aldiener <aldiener@htwg-konstanz.de>
2012-11-19 11:04:04 +0100

    implemented hashcode and equals

--- plugininterface/src/main/java/de/fhkn/in/uce/plugininterface/message/NATTraversalTechniqueAttribute.java
+++ plugininterface/src/main/java/de/fhkn/in/uce/plugininterface/message/NATTraversalTechniqueAttribute.java
@@ -59,6 +59,28 @@ public class NATTraversalTechniqueAttribute implements Attribute {
         out.flush();
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + encoded;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        NATTraversalTechniqueAttribute other = (NATTraversalTechniqueAttribute) obj;
+        if (encoded != other.encoded)
+            return false;
+        return true;
+    }
+
     /**
      * Creates a {@link NATTraversalTechniqueAttribute} from the given encoding.
      * 
