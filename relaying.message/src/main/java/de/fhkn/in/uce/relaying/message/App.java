74d6564d221a439cee049b9401d3d37a23baa128
aldiener <aldiener@htwg-konstanz.de>
2012-11-22 16:16:43 +0100

    extracted custom relaying messages to reuse it

---
+++ relaying.message/src/main/java/de/fhkn/in/uce/relaying/message/App.java
@@ -0,0 +1,13 @@
+package de.fhkn.in.uce.relaying.message;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
