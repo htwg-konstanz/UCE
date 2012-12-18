7fc4fa2d55d369795934b8d6f5905525aadb5a2d
aldiener <aldiener@htwg-konstanz.de>
2012-12-18 15:51:03 +0100

    information for hole punching process will be sent after a connection request, mediator calls target to start hp

--- holepunching.mediator/src/main/java/de/fhkn/in/uce/holepunching/mediator/HolePunchingConnRequestHandler.java
+++ holepunching.mediator/src/main/java/de/fhkn/in/uce/holepunching/mediator/HolePunchingConnRequestHandler.java
@@ -16,32 +16,30 @@
  */
 package de.fhkn.in.uce.holepunching.mediator;
 
-import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Inet6Address;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.fhkn.in.uce.holepunching.message.HolePunchingAttribute;
-import de.fhkn.in.uce.holepunching.message.HolePunchingMethod;
+import de.fhkn.in.uce.mediator.connectionhandling.ConnectionRequest;
+import de.fhkn.in.uce.mediator.connectionhandling.ConnectionRequestList;
 import de.fhkn.in.uce.mediator.peerregistry.UserData;
 import de.fhkn.in.uce.mediator.peerregistry.UserList;
 import de.fhkn.in.uce.mediator.util.MediatorUtil;
 import de.fhkn.in.uce.plugininterface.mediator.HandleMessage;
 import de.fhkn.in.uce.plugininterface.message.NATTraversalTechniqueAttribute;
 import de.fhkn.in.uce.stun.attribute.ErrorCode.STUNErrorCode;
+import de.fhkn.in.uce.stun.attribute.Token;
 import de.fhkn.in.uce.stun.attribute.Username;
 import de.fhkn.in.uce.stun.attribute.XorMappedAddress;
 import de.fhkn.in.uce.stun.header.STUNMessageClass;
 import de.fhkn.in.uce.stun.header.STUNMessageMethod;
 import de.fhkn.in.uce.stun.message.Message;
-import de.fhkn.in.uce.stun.message.MessageReader;
 import de.fhkn.in.uce.stun.message.MessageStaticFactory;
 
 /**
@@ -55,12 +53,25 @@ import de.fhkn.in.uce.stun.message.MessageStaticFactory;
  */
 public final class HolePunchingConnRequestHandler implements HandleMessage {
     private static final Logger logger = LoggerFactory.getLogger(HolePunchingConnRequestHandler.class);
+    private final ConnectionRequestList connectionRequests = ConnectionRequestList.INSTANCE;
     private final MediatorUtil mediatorUtil = MediatorUtil.INSTANCE;
     private final UserList userList = UserList.INSTANCE;
 
     @Override
     public void handleMessage(final Message message, final Socket controlConnection) throws Exception {
+        if (message.getMessageClass() == STUNMessageClass.REQUEST) {
+            logger.debug("Handling connection request from {}", controlConnection.toString());
+            this.handleConnectionRequest(message, controlConnection);
+        } else if (message.getMessageClass() == STUNMessageClass.SUCCESS_RESPONSE) {
+            logger.debug("Handling connection request response from {}", controlConnection.toString());
+            this.handleConnectionRequestResponse(message, controlConnection);
+        }
+    }
+
+    private void handleConnectionRequest(final Message message, final Socket controlConnection) throws Exception {
         this.mediatorUtil.checkForAttribute(message, Username.class);
+        this.mediatorUtil.checkForAttribute(message, XorMappedAddress.class);
+        this.connectionRequests.putConnectionRequest(new ConnectionRequest(controlConnection, message));
         final Username username = message.getAttribute(Username.class);
         final UserData user = this.userList.getUserDataByUserId(username.getUsernameAsString());
         if (user == null) {
@@ -68,65 +79,39 @@ public final class HolePunchingConnRequestHandler implements HandleMessage {
             this.sendFailureResponse(message, errorMessage, STUNErrorCode.BAD_REQUEST,
                     controlConnection.getOutputStream());
         } else {
-            this.sendHPMessagesToPeers(user, message, controlConnection);
+            this.forwardConnectionRequest(user, message, controlConnection);
+            // this.sendHPMessagesToPeers(user, message, controlConnection);
         }
     }
 
-    private InetSocketAddress callTarget(final UserData target) throws Exception {
+    private void handleConnectionRequestResponse(final Message message, final Socket controlConnection)
+            throws Exception {
+        final ConnectionRequest connReq = this.connectionRequests.getConnectionRequest(new String(message.getHeader()
+                .getTransactionId()));
+        final Message successResponse = connReq.getConnectionRequestMessage().buildSuccessResponse();
+        // public endpoint
+        successResponse.addAttribute(this.createXorMappedAddressFromSocket(controlConnection, successResponse
+                .getHeader().getTransactionId()));
+        // private endpoint
+        successResponse.addAttribute(message.getAttribute(XorMappedAddress.class));
+        logger.debug("Sending connection request response to {}", connReq.getControlConnection().toString()); //$NON-NLS-1$
+        successResponse.writeTo(connReq.getControlConnection().getOutputStream());
+    }
+
+    private void forwardConnectionRequest(final UserData target, final Message connectionRequestFromSource,
+            final Socket controlConnectionToSource) throws Exception {
         final Socket toTarget = target.getSocketToUser();
         final Message connectionRequest = MessageStaticFactory.newSTUNMessageInstance(STUNMessageClass.REQUEST,
-                STUNMessageMethod.CONNECTION_REQUEST);
+                STUNMessageMethod.CONNECTION_REQUEST, connectionRequestFromSource.getHeader().getTransactionId());
         connectionRequest.addAttribute(new HolePunchingAttribute());
+        connectionRequest.addAttribute(connectionRequestFromSource.getAttribute(Token.class));
+        // public endpoint
+        connectionRequest.addAttribute(this.createXorMappedAddressFromSocket(controlConnectionToSource,
+                connectionRequest.getHeader().getTransactionId()));
+        // private endpoint
+        connectionRequest.addAttribute(connectionRequestFromSource.getAttribute(XorMappedAddress.class));
+        logger.debug("Forwarding endpoints to target {}", toTarget.toString()); //$NON-NLS-1$
         connectionRequest.writeTo(toTarget.getOutputStream());
-        return this.waitForTarget(toTarget);
-    }
-
-    private InetSocketAddress waitForTarget(final Socket toTarget) throws Exception {
-        final Message responseFromTarget = MessageReader.createMessageReader().readSTUNMessage(
-                toTarget.getInputStream());
-        if (responseFromTarget.isSuccessResponse() && responseFromTarget.hasAttribute(XorMappedAddress.class)) {
-            return responseFromTarget.getAttribute(XorMappedAddress.class).getEndpoint();
-        } else {
-            throw new Exception("Target could not be started"); //$NON-NLS-1$
-        }
-    }
-
-    private void sendHPMessagesToPeers(final UserData user, final Message requestMessage, final Socket socketToSource)
-            throws Exception {
-        final List<InetSocketAddress> addressesOfSource = new ArrayList<InetSocketAddress>();
-        addressesOfSource.add(requestMessage.getAttribute(XorMappedAddress.class).getEndpoint());
-        addressesOfSource.add(new InetSocketAddress(socketToSource.getInetAddress(), socketToSource.getPort()));
-        final Message toTarget = this.createHPMessageWithAddresses(addressesOfSource);
-        final InetSocketAddress privateAddressOfTarget = this.callTarget(user);
-        final List<InetSocketAddress> addressesOfTarget = new ArrayList<InetSocketAddress>();
-        addressesOfTarget.add(privateAddressOfTarget);
-        addressesOfTarget.add(new InetSocketAddress(user.getSocketToUser().getInetAddress(), user.getSocketToUser()
-                .getPort()));
-        final Message toSource = this.createHPMessageWithAddresses(addressesOfTarget);
-        this.sendMessage(toTarget, user.getSocketToUser());
-        this.sendMessage(toSource, socketToSource);
-    }
-
-    private void sendMessage(final Message toSend, final Socket socket) throws IOException {
-        synchronized (socket.getOutputStream()) {
-            toSend.writeTo(socket.getOutputStream());
-        }
-    }
-
-    private Message createHPMessageWithAddresses(final List<InetSocketAddress> addresses) {
-        final Message result = MessageStaticFactory.newSTUNMessageInstance(STUNMessageClass.REQUEST,
-                HolePunchingMethod.FORWARDED_ENDPOINTS);
-        for (InetSocketAddress address : addresses) {
-            if (address.getAddress() instanceof Inet6Address) {
-                final XorMappedAddress xorAddressIpv6 = new XorMappedAddress(address, ByteBuffer.wrap(
-                        result.getHeader().getTransactionId()).getInt());
-                result.addAttribute(xorAddressIpv6);
-            } else {
-                final XorMappedAddress xorAddressIpv4 = new XorMappedAddress(address);
-                result.addAttribute(xorAddressIpv4);
-            }
-        }
-        return result;
     }
 
     private void sendFailureResponse(final Message message, final String errorReaon, final STUNErrorCode errorCode,
@@ -134,6 +119,17 @@ public final class HolePunchingConnRequestHandler implements HandleMessage {
         logger.debug(errorReaon);
         final Message failureResponse = message.buildFailureResponse(errorCode, errorReaon);
         failureResponse.writeTo(out);
+    }
+
+    private XorMappedAddress createXorMappedAddressFromSocket(final Socket socket, final byte[] transactionid) {
+        XorMappedAddress result;
+        if (socket.getInetAddress() instanceof Inet6Address) {
+            result = new XorMappedAddress(new InetSocketAddress(socket.getInetAddress(), socket.getPort()), ByteBuffer
+                    .wrap(transactionid).getInt());
+        } else {
+            result = new XorMappedAddress(new InetSocketAddress(socket.getInetAddress(), socket.getPort()));
+        }
+        return result;
     }
 
     @Override
